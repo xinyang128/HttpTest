57c2989eab42b398eaf299fa188b6d61841c3c73
xinfeng128 <150389523@qq.com>
2015-11-16 15:43:07 +0800

    Initial Commit

---
+++ HttpTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HttpTest
+//
+//  Created by wanglong on 15/11/16.
+//  Copyright © 2015年 wanglong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
